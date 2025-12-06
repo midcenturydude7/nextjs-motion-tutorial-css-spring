603f7f1e749a9953508164286a6d332c9079d518
midCenturyDude7 <mgriffes@gmail.com>
2025-12-06 13:40:18 -0800

    Added styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
+
+import React from "react";
+
 export default function Home() {
+  const [state, setState] = React.useState(false);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="max-w-2xl p-8">
