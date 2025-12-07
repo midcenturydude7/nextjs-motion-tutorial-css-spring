7578fd7e2620b42a3bf36cae25f4f40f4e18420b
midCenturyDude7 <mgriffes@gmail.com>
2025-12-07 15:26:35 -0800

    Working on converting css to tailwindcss

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -1,46 +1,44 @@
 import { spring } from "motion";
 
-export default function Box({ state, setState }: { state: boolean; setState: (state: boolean) => void }) {
+export default function Box({
+  state,
+  setState,
+}: {
+  state: boolean;
+  setState: (state: boolean) => void;
+}) {
   return (
-    <div>
-      <div className="box" data-state={state} />
+    <div className="example-container flex flex-col items-center justify-center gap-5">
+      <div
+        className="box h-[100px] w-[100px] rounded-[10px] bg-[#8df0cc]"
+        data-state={state}
+      />
       <button onClick={() => setState(!state)}>Toggle Position</button>
       <style>
         {`
-                    .example-container {
-                        display: flex;
-                        flex-direction: column;
-                        align-items: center;
-                        justify-content: center;
-                        gap: 20px;
-                    }
+            .box {
+                transition: transform ${spring(0.5, 0.8)};
+                transform: translateX(-100%);
+            }
 
-                    .example-container .box {
-                        width: 100px;
-                        height: 100px;
-                        background-color: #8df0cc;
-                        border-radius: 10px;
-                        transition: transform ${spring(0.5, 0.8)};
-                        transform: translateX(-100%);
-                    }
+            .example-container .box[data-state="true"] {
+                transform: translateX(100%) rotate(180deg);
+            }
 
-                    .example-container .box[data-state="true"] {
-                        transform: translateX(100%) rotate(180deg);
-                    }
+            .example-container button {
+                background-color: #8df0cc;
+                color: #0f1115;
+                border-radius: 5px;
+                padding: 10px;
+                margin: 10px;
+                transition: background-color 0.75s;
+                cursor: pointer;
+            }
 
-                    .example-container button {
-                        background-color: #8df0cc;
-                        color: #0f1115;
-                        border-radius: 5px;
-                        padding: 10px;
-                        margin: 10px;
-                        transition: background-color 0.3s;
-                        cursor: pointer;
-                    }
-                        button:hover {
-                            background-color: #72cbb6;
-                        }
-                `}
+            .example-container button:hover {
+                    background-color: #38b497;
+            }
+        `}
       </style>
     </div>
   );
