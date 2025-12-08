78fb9a912266ffc1893deeb228a96684d7605849
midCenturyDude7 <mgriffes@gmail.com>
2025-12-07 19:21:09 -0800

    Replaced inline css styles with Tailwind CSS where doable

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -13,31 +13,22 @@ export default function Box({
         className="box h-[100px] w-[100px] rounded-[10px] bg-[#8df0cc]"
         data-state={state}
       />
-      <button onClick={() => setState(!state)}>Toggle Position</button>
+      <button
+        className="m-2 cursor-pointer rounded bg-[#8df0cc] p-2 text-gray-900 hover:bg-[#38b497]"
+        onClick={() => setState(!state)}
+      >
+        Toggle Position
+      </button>
       <style>
         {`
-            .box {
-                transition: transform ${spring(0.5, 0.8)};
-                transform: translateX(-100%);
-            }
+          .box {
+              transition: transform ${spring(0.5, 0.8)};
+              transform: translateX(-100%);
+          }
 
-            .example-container .box[data-state="true"] {
-                transform: translateX(100%) rotate(180deg);
-            }
-
-            .example-container button {
-                background-color: #8df0cc;
-                color: #0f1115;
-                border-radius: 5px;
-                padding: 10px;
-                margin: 10px;
-                transition: background-color 0.75s;
-                cursor: pointer;
-            }
-
-            .example-container button:hover {
-                    background-color: #38b497;
-            }
+          .example-container .box[data-state="true"] {
+              transform: translateX(100%) rotate(180deg);
+          }
         `}
       </style>
     </div>
